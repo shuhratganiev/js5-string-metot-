630bd612bf04f3a547e5a9fe1d33d3e7804b96a4
khudaibergenovaasal <what88370@gmail.com>
2024-11-20 18:23:01 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let paydalanuvchi = prompt("Ism - Familya")
+let  savol = prompt("katta xarf +. kichik xarf-")
+if (savol === '+') {
+    alert(paydalanuvchi.toLocaleUpperCase())
+}
+ else{
+    alert(paydalanuvchi.toLocaleLowerCase())
+}
